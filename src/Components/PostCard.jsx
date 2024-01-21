b7cf30aab635e09e9a66a70abc08b447563fb39c
shivansh13579 <shivansh0975@gmail.com>
2024-01-21 21:45:22 +0530

    create components

---
+++ src/Components/PostCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import appwriteService from '../appwrite/config'
+import { Link } from 'react-router-dom'
+
+
+function PostCard({$id,title,featuredImage}) {
+  return (
+    <div>
+      PostCard
+    </div>
+  )
+}
+
+export default PostCard
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,9 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
+import Container from "./container/container";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
     Footer,
-    Logo
+    Logo,
+    Container,
+    LogoutBtn
 }
