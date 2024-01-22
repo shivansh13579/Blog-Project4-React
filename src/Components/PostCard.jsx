2253eb8e11bcb87c36e50fedf1fa0732759a7ed9
shivansh13579 <shivansh0975@gmail.com>
2024-01-22 22:03:15 +0530

    create login and signup components

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -4,11 +4,16 @@ import { Link } from 'react-router-dom'
 
 
 function PostCard({$id,title,featuredImage}) {
-  return (
-    <div>
-      PostCard
-    </div>
-  )
+    <Link to={`/post/${$id}`}>
+       <div className='w-full bg-gray-100 rounded-xl p-4'>
+        <div className='w-full justify-center mb-4'>
+             <img src={appwriteService.getFilePreview(featuredImage)} alt={title} 
+              className='rounded-xl'
+             />
+        </div>
+        <h2>{title}</h2>
+       </div>
+    </Link>
 }
 
 export default PostCard
