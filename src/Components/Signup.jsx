03d0bdf181557ffbe12b400dcbe7ebf6ad8577ed
shivansh13579 <shivansh0975@gmail.com>
2024-01-23 15:55:37 +0530

    create authLayout

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -34,6 +34,52 @@ function Signup() {
                      <Logo width='100%'/>
                 </span>
         </div>
+        <h2 className='text-center text-2xl font-bold leading-tight'>Sign up to create account</h2>
+        <p className='mt-2 text-center text-base text-black/60'>
+                Already have an account?&nbsp;
+                <Link 
+                to="/login"
+                className='font-medium text-primary transition-all duration-200 hover:underline'
+                >
+                  Sign In 
+                </Link>
+        </p>
+        {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
+          <div className='space-y-5'>
+            <Input
+              label="Full Name: "
+              type='text'
+              placeholder='Enter your full name'
+              {...register("name",{
+                required: true
+              })}
+            />
+            <Input
+               label="Email: "
+               placeholder='Enter your email'
+               type='email'
+               {...register("email",{
+                required: true
+               })}
+            />
+            <Input
+              label="password: "
+              placeholder="Enter you password"
+              type="password"
+              {...register("password",{
+                required: true
+              })}
+            />
+            <Button
+            type="submit"
+            className="w-full"
+            >
+              Create Account
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   )
