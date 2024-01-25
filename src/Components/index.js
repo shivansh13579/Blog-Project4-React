2d259fddef000e701ac2e8017322c2df1023a7b4
shivansh13579 <shivansh0975@gmail.com>
2024-01-25 16:31:57 +0530

    crate signup and login pages create

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "./Signup";
+import Login from "./Login";
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Signup,
+    Login
 }
