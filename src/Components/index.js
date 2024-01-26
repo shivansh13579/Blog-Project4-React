e1c47b51d9801c019c49090fc3dcc5d3f3aaaa5e
shivansh13579 <shivansh0975@gmail.com>
2024-01-26 21:55:50 +0530

    create post page and addPost page

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,6 +5,11 @@ import Container from "./container/container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Signup from "./Signup";
 import Login from "./Login";
+import PostCard from './PostCard';
+import RTE from './RTE';
+import Select from './Select';
+import Input from './Input';
+import PostForm from './post-form/PostForm';
 
 export {
     Header,
@@ -13,5 +18,10 @@ export {
     Container,
     LogoutBtn,
     Signup,
-    Login
+    Login,
+    PostCard,
+    RTE,
+    Select,
+    Input,
+    PostForm
 }
